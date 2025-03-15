e3294c176527833e0a05a401cee2b039516112f4
jeongdonghee <dh.jeong@ex-em.com>
2025-03-16 04:05:56 +0900

    2장. 테스트
    - jUnit 적용
    - addAndGet 테스트
    - 테스트 일관성 적용 (deleteAll, count)
    - 잘못된 get 테스트

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -1,29 +1,70 @@
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    @Test
+    public void addAndGet() throws SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
-        User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        User user1 = new User("weg9511", "정동희", "1q2w3e4r!");
+        User user2 = new User("weg9512", "정동흐", "1q2w3e4r@");
 
-        dao.add(user);
+        dao.deleteAll();
+        assertEquals(dao.getCount(), 0);
 
-        System.out.println(user.getId() + " 등록 성공");
+        dao.add(user1);
+        dao.add(user2);
+        assertEquals(dao.getCount(), 2);
 
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
+        User userget1 = dao.get(user1.getId());
+        assertEquals(userget1.getName(), user1.getName());
+        assertEquals(userget1.getPassword(), user1.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공");
+        User userge2 = dao.get(user2.getId());
+        assertEquals(userge2.getName(), userge2.getName());
+        assertEquals(userge2.getPassword(), userge2.getPassword());
+    }
 
-        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
-        System.out.println("Connection counter: " + ccm.getCounter());
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("user1", "유저1", "password1");
+        User user2 = new User("user2", "유저2", "password2");
+        User user3 = new User("user3", "유저3", "password3");
+
+        dao.deleteAll();
+        assertEquals(dao.getCount(), 0);
+
+        dao.add(user1);
+        assertEquals(dao.getCount(), 1);
+
+        dao.add(user2);
+        assertEquals(dao.getCount(), 2);
+
+        dao.add(user3);
+        assertEquals(dao.getCount(), 3);
+    }
+
+    @Test
+    public void getUserFailure() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        dao.deleteAll();
+        assertEquals(dao.getCount(), 0);
+
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            dao.get("unknown_id");
+        });
     }
 }
