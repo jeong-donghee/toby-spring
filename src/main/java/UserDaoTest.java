f70eafab8b06d5057cf2792720fd873ea3c184d3
jeongdonghee <dh.jeong@ex-em.com>
2025-03-16 23:24:32 +0900

    테스트 코드 리팩토링 : 스프링 컨테이너 DI 활용

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -1,23 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
+    @Autowired
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    public void setUp() {
+        user1 = new User("weg9511", "정동희", "1q2w3e4r!");
+        user2 = new User("weg9512", "정동흐", "1q2w3e4r@");
+        user3 = new User("weg   9513", "정동히", "1q2w3e4r#");
+    }
+
     @Test
     public void addAndGet() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user1 = new User("weg9511", "정동희", "1q2w3e4r!");
-        User user2 = new User("weg9512", "정동흐", "1q2w3e4r@");
-
         dao.deleteAll();
         assertEquals(dao.getCount(), 0);
 
@@ -29,20 +40,13 @@ public class UserDaoTest {
         assertEquals(userget1.getName(), user1.getName());
         assertEquals(userget1.getPassword(), user1.getPassword());
 
-        User userge2 = dao.get(user2.getId());
-        assertEquals(userge2.getName(), userge2.getName());
-        assertEquals(userge2.getPassword(), userge2.getPassword());
+        User userget2 = dao.get(user2.getId());
+        assertEquals(userget2.getName(), user2.getName());
+        assertEquals(userget2.getPassword(), user2.getPassword());
     }
 
     @Test
     public void count() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        User user1 = new User("user1", "유저1", "password1");
-        User user2 = new User("user2", "유저2", "password2");
-        User user3 = new User("user3", "유저3", "password3");
-
         dao.deleteAll();
         assertEquals(dao.getCount(), 0);
 
@@ -58,8 +62,6 @@ public class UserDaoTest {
 
     @Test
     public void getUserFailure() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
         dao.deleteAll();
         assertEquals(dao.getCount(), 0);
 
